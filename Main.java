cd748fc9a5ba68f8047575e0d7ff700c8a52bcb3
emrecanpolat <35192029+emrecanpolat@users.noreply.github.com>
2023-08-31 12:57:37 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Wall littleWall = new Wall();
+        littleWall.setWidth(85);
+        littleWall.setHeight(6);
+
+        System.out.println(littleWall.getArea());
+
+
+    }
+
+
+
+}
+
+
